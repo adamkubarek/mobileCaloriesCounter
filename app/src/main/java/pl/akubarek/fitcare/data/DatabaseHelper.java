e08765948c11139e2a15e78e8836089a17cffef4
AdamK <Adamkubarek@gmail.com>
2017-03-26 00:36:04 +0100

    AddProductDialog added

--- app/src/main/java/pl/akubarek/fitcare/data/DatabaseHelper.java
+++ app/src/main/java/pl/akubarek/fitcare/data/DatabaseHelper.java
@@ -1,47 +1,47 @@
-package pl.akubarek.fitcare.data;
-
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
-
-import pl.akubarek.fitcare.util.Constants;
-
-/**
- * Created by BloodyFire on 25.03.2017.
- */
-
-public class DatabaseHelper extends SQLiteOpenHelper {
-
-    private final static int DB_VERSION = 1;
-    private final static String DB_NAME = "fitcare.db";
-
-    private final static String TAG = DatabaseHelper.class.getSimpleName();
-
-    public DatabaseHelper(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_PRODUCT_TABLE);
-
-        Log.d(TAG, "Product list table created");
-        Log.d(TAG, CREATE_PRODUCT_TABLE);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-    }
-
-    private static final String CREATE_PRODUCT_TABLE =
-            "CREATE TABLE " + Constants.PRODUCT_TABLE + "("
-                    + Constants.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + Constants.COLUMN_NAME + " TEXT, "
-                    + Constants.COLUMN_CATEGORY + " TEXT, "
-                    + Constants.COLUMN_WEIGHT + " INTEGER, "
-                    + Constants.COLUMN_CALORIES + " INTEGER, "
-                    + Constants.COLUMN_PROTEIN + " NUMERIC, "
-                    + Constants.COLUMN_FAT + " NUMERIC, "
-                    + Constants.COLUMN_CARBS + " NUMERIC);";
-}
+//package pl.akubarek.fitcare.data;
+//
+//import android.content.Context;
+//import android.database.sqlite.SQLiteDatabase;
+//import android.database.sqlite.SQLiteOpenHelper;
+//import android.util.Log;
+//
+//import pl.akubarek.fitcare.util.Constants;
+//
+///**
+// * Created by BloodyFire on 25.03.2017.
+// */
+//
+//public class DatabaseHelper extends SQLiteOpenHelper {
+//
+//    private final static int DB_VERSION = 1;
+//    private final static String DB_NAME = "fitcare.db";
+//
+//    private final static String TAG = DatabaseHelper.class.getSimpleName();
+//
+//    public DatabaseHelper(Context context) {
+//        super(context, DB_NAME, null, DB_VERSION);
+//    }
+//
+//    @Override
+//    public void onCreate(SQLiteDatabase db) {
+//        db.execSQL(CREATE_PRODUCT_TABLE);
+//
+//        Log.d(TAG, "Product list table created");
+//        Log.d(TAG, CREATE_PRODUCT_TABLE);
+//    }
+//
+//    @Override
+//    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+//    }
+//
+//    private static final String CREATE_PRODUCT_TABLE =
+//            "CREATE TABLE " + Constants.PRODUCT_TABLE + "("
+//                    + Constants.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+//                    + Constants.COLUMN_NAME + " TEXT, "
+//                    + Constants.COLUMN_CATEGORY + " TEXT, "
+//                    + Constants.COLUMN_WEIGHT + " INTEGER, "
+//                    + Constants.COLUMN_CALORIES + " INTEGER, "
+//                    + Constants.COLUMN_PROTEIN + " NUMERIC, "
+//                    + Constants.COLUMN_FAT + " NUMERIC, "
+//                    + Constants.COLUMN_CARBS + " NUMERIC);";
+//}
--- app/src/main/java/pl/akubarek/fitcare/ui/productList/ProductListActivity.java
+++ app/src/main/java/pl/akubarek/fitcare/ui/productList/ProductListActivity.java
@@ -1,22 +1,28 @@
 package pl.akubarek.fitcare.ui.productList;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import pl.akubarek.fitcare.R;
 import pl.akubarek.fitcare.model.Product;
+
 
 public class ProductListActivity extends AppCompatActivity {
 
@@ -38,13 +44,15 @@ public class ProductListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        productListView = (ListView) findViewById(R.id.product_list_view);
+        products = new ArrayList<>();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                displayAddProductDialog();
+                refreshStaticList();
             }
         });
     }
@@ -69,5 +77,62 @@ public class ProductListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void displayAddProductDialog () {
+        final Dialog dialog = new Dialog(context);
+
+        dialog.setContentView(R.layout.add_product_dialog);
+
+        // find dialog fields;
+        TextView textName = (TextView) dialog.findViewById(R.id.textName);
+        TextView textCategory = (TextView) dialog.findViewById(R.id.textCategory);
+        TextView textCalories = (TextView) dialog.findViewById(R.id.textCalories);
+        TextView textProtein = (TextView) dialog.findViewById(R.id.textProtein);
+        TextView textCarbs = (TextView) dialog.findViewById(R.id.textCarbs);
+        TextView textFat = (TextView) dialog.findViewById(R.id.textFat);
+        final EditText editName = (EditText) dialog.findViewById(R.id.editName);
+        final EditText editCategory = (EditText) dialog.findViewById(R.id.editCategory);
+        final EditText editCalories = (EditText) dialog.findViewById(R.id.editCalories);
+        final EditText editProtein = (EditText) dialog.findViewById(R.id.editProtein);
+        final EditText editCarbs = (EditText) dialog.findViewById(R.id.editCarbs);
+        final EditText editFat = (EditText) dialog.findViewById(R.id.editFat);
+
+        Button buttonAdd = (Button) dialog.findViewById(R.id.button_add);
+        Button buttonCancel = (Button) dialog.findViewById(R.id.button_cancel);
+
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editName.getText().toString().isEmpty() && !editCategory.getText().toString().isEmpty() && !editCalories.getText().toString().isEmpty() &&
+                        !editProtein.getText().toString().isEmpty() && !editCarbs.getText().toString().isEmpty() && !editFat.getText().toString().isEmpty()) {
+                    String name = editName.getText().toString();
+                    String category = editCategory.getText().toString();
+                    int calories = Integer.valueOf(editCalories.getText().toString());
+                    double protein = Double.valueOf(editProtein.getText().toString());
+                    double carbs = Double.valueOf(editCarbs.getText().toString());
+                    double fat = Double.valueOf(editFat.getText().toString());
+
+                    // !!!!!!!!!!!!!!!!  DODAWANIE OBIEKTU DO BAZY
+                    Product product = new Product(name, category, calories, protein, carbs, fat);
+                    addProductToDatabase();
+
+
+                    Toast.makeText(context, name + "\n" + category + "\n" + product.getWeight() + "\n" + calories + "\n" + protein + "\n" + carbs + "\n" + fat, Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+                } else {
+                    Toast.makeText(context, "Uzupełnij odpowiednio wszystkie pola", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        dialog.show();
     }
 }
